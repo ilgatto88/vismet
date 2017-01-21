6cf5af2c6087bdb85bc30e5c1b20fe392607c3ea
ilgatto88 <tjanos88@gmail.com>
2017-01-21 01:40:35 +0100

    Try/except cycles updated

--- database_check.py
+++ database_check.py
@@ -10,89 +10,95 @@ now_time = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
 #Counting errors
 error_count = 0
 
+#database check parameter
+dbcheck = True
+
 #Fetching login data from text file
-cred_list = []
-with open('/home/pi/Documents/logfiles/login_metgrab_database.txt', 'r') as loginfile:
-    for item in loginfile.read().split(','):
-        cred_list.append(item)
+try:
+    cred_list = []
+    with open('/home/pi/Documents/logfiles/login_metgrab_databas.txt', 'r') as loginfile:
+        for item in loginfile.read().split(','):
+            cred_list.append(item)
 
-user = cred_list[0]
-pword = cred_list[1]
+    user = cred_list[0]
+    pword = cred_list[1]
+
 	
-#Try to connect to the PostgreSQL database
-try:
-	dsn = "dbname='met_project' user='"+user+"' host = 'localhost' password='"+pword+"'"
-	conn = psycopg2.connect(dsn)
-	print('Successful login!')
-	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    #Connecting to the PostgreSQL database
+    dsn = "dbname='met_project' user='"+user+"' host = 'localhost' password='"+pword+"'"
+    conn = psycopg2.connect(dsn)
+    print('Successful login!')
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
-except Exception as e1:
-    print(str(e1))
-    print('Unable to connect to the database!')
-    error_count += 1
-
-# Define a cursor to work with
-cur = conn.cursor()
-
-#######Fetching OMSZ data#######
-try:
-    sql_omsz = "SELECT timeofrequest, Fcst_Tmin_for_day1, Fcst_Tmax_for_day1 FROM omsz_table ORDER BY timeofrequest DESC LIMIT 1;"
-    cur.execute(sql_omsz)
-    fetched_data_omsz = cur.fetchall()
+    # Define a cursor to work with
+    cur = conn.cursor()
 except:
-    print("Problem with OMSZ data, cannot fetch last row!")
-    error_count += 1
+    error_count += 101
+    print('Database not available!')
+    dbcheck = False
 
-#######Fetching Idokep data#######
-try:
-    sql_idokep = "SELECT timeofrequest, Fcst_Tmin_for_day1, Fcst_Tmax_for_day1 FROM idokep_table ORDER BY timeofrequest DESC LIMIT 1;"
-    cur.execute(sql_idokep)
-    fetched_data_idokep = cur.fetchall()
-except:
-    print("Problem with Idokep data, cannot fetch last row!")
-    error_count += 1
+#If dbcheck = False, this part won't be executed
+if dbcheck:
 
-#######Fetching Koponyeg data#######
-try:
-    sql_koponyeg = "SELECT timeofrequest, Fcst_Tmin_for_day1, Fcst_Tmax_for_day1 FROM koponyeg_table ORDER BY timeofrequest DESC LIMIT 1;"
-    cur.execute(sql_koponyeg)
-    fetched_data_koponyeg = cur.fetchall()
-except:
-    print("Problem with Koponyeg data, cannot fetch last row!")
-    error_count += 1
+    #######Fetching OMSZ data#######
+    try:
+        sql_omsz = "SELECT timeofrequest, Fcst_Tmin_for_day1, Fcst_Tmax_for_day1 FROM omsz_table ORDER BY timeofrequest DESC LIMIT 1;"
+        cur.execute(sql_omsz)
+        fetched_data_omsz = cur.fetchall()
+    except:
+        print("Problem with OMSZ data, cannot fetch last row!")
+        error_count += 1
 
-#######Fetching Ogimet data#######
-try:
-    sql_ogimet = "SELECT timeofrequest, Obs_Tmin, Obs_Tmax FROM ogimet_table ORDER BY timeofrequest DESC LIMIT 1;"
-    cur.execute(sql_ogimet)
-    fetched_data_ogimet = cur.fetchall()
-except:
-    print("Problem with Ogimet data, cannot fetch last row!")
-    error_count += 1
+    #######Fetching Idokep data#######
+    try:
+        sql_idokep = "SELECT timeofrequest, Fcst_Tmin_for_day1, Fcst_Tmax_for_day1 FROM idokep_table ORDER BY timeofrequest DESC LIMIT 1;"
+        cur.execute(sql_idokep)
+        fetched_data_idokep = cur.fetchall()
+    except:
+        print("Problem with Idokep data, cannot fetch last row!")
+        error_count += 1
 
-#######Checking date of the last row of omsz_table#######
-if (now_time == fetched_data_omsz[0][0][0:10]) and (fetched_data_omsz[0][1] != None) and (fetched_data_omsz[0][2] != None):
-    print('OMSZ OK!')
-else:
-    error_count += 1
+    #######Fetching Koponyeg data#######
+    try:
+        sql_koponyeg = "SELECT timeofrequest, Fcst_Tmin_for_day1, Fcst_Tmax_for_day1 FROM koponyeg_table ORDER BY timeofrequest DESC LIMIT 1;"
+        cur.execute(sql_koponyeg)
+        fetched_data_koponyeg = cur.fetchall()
+    except:
+        print("Problem with Koponyeg data, cannot fetch last row!")
+        error_count += 1
 
-#Checking date of the last row of idokep_table#######
-if (now_time == fetched_data_idokep[0][0][0:10]) and (fetched_data_idokep[0][1] != None) and (fetched_data_idokep[0][2] != None):
-    print('Idokep OK!')
-else:
-    error_count += 1
+    #######Fetching Ogimet data#######
+    try:
+        sql_ogimet = "SELECT timeofrequest, Obs_Tmin, Obs_Tmax FROM ogimet_table ORDER BY timeofrequest DESC LIMIT 1;"
+        cur.execute(sql_ogimet)
+        fetched_data_ogimet = cur.fetchall()
+    except:
+        print("Problem with Ogimet data, cannot fetch last row!")
+        error_count += 1
 
-#Checking date of the last row of koponyeg_table#######
-if (now_time == fetched_data_koponyeg[0][0][0:10]) and (fetched_data_koponyeg[0][1] != None) and (fetched_data_koponyeg[0][2] != None):
-    print('Koponyeg OK!')
-else:
-    error_count += 1
+    #######Checking date of the last row of omsz_table#######
+    if (now_time == fetched_data_omsz[0][0][0:10]) and (fetched_data_omsz[0][1] != None) and (fetched_data_omsz[0][2] != None):
+        print('OMSZ OK!')
+    else:
+        error_count += 1
 
-#Checking date of the last row of ogimet_table#######
-if (now_time == fetched_data_ogimet[0][0][0:10]) and (fetched_data_ogimet[0][1] != None) and (fetched_data_ogimet[0][2] != None):
-    print('Ogimet OK!')
-else:
-    error_count += 1
+    #Checking date of the last row of idokep_table#######
+    if (now_time == fetched_data_idokep[0][0][0:10]) and (fetched_data_idokep[0][1] != None) and (fetched_data_idokep[0][2] != None):
+        print('Idokep OK!')
+    else:
+        error_count += 1
+
+    #Checking date of the last row of koponyeg_table#######
+    if (now_time == fetched_data_koponyeg[0][0][0:10]) and (fetched_data_koponyeg[0][1] != None) and (fetched_data_koponyeg[0][2] != None):
+        print('Koponyeg OK!')
+    else:
+        error_count += 1
+
+    #Checking date of the last row of ogimet_table#######
+    if (now_time == fetched_data_ogimet[0][0][0:10]) and (fetched_data_ogimet[0][1] != None) and (fetched_data_ogimet[0][2] != None):
+        print('Ogimet OK!')
+    else:
+        error_count += 1
 
 print('Number of errors: ' + str(error_count))
 
@@ -110,7 +116,10 @@ if error_count > 0:
 
     send_to = cred_list_gmail[0]
     mail_subject = 'metgrab.py warning message'
-    mail_text = 'Database is missing some data, please check it! ' + 'Number of errors: ' + str(error_count)
+    if error_count < 100:
+        mail_text = 'Database is missing some data, please check it! ' + 'Number of wrong or incomplete tables: ' + str(error_count)
+    else:
+        mail_text = 'Database connection problem! Please check every table.'
 
     #GMail creditentials
     gmail_sender = cred_list_gmail[0]
@@ -132,6 +141,6 @@ if error_count > 0:
 
     #Sending the mail
     smtpObj.sendmail(gmail_sender, [send_to], mail_body)
-    print('Warning: e-mail sent')
+    print('Warning e-mail sent')
 
     smtpObj.quit()
